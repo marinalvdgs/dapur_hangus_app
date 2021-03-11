9a577f219ec2199e4023ca15711b9176413ce9e1
marinalvdgs <marinalvdgs@gmail.com>
2021-03-11 14:24:39 +0600

    first implementation of dish card ui

--- lib/screens/receipt_book_page.dart
+++ lib/screens/receipt_book_page.dart
@@ -1,3 +1,5 @@
+import 'package:dapur_hangus_app/theme.dart';
+import 'package:dapur_hangus_app/ui/dh_dish_card.dart';
 import 'package:flutter/material.dart';
 
 class ReceiptBookPage extends StatefulWidget {
@@ -8,6 +10,18 @@ class ReceiptBookPage extends StatefulWidget {
 class _ReceiptBookPageState extends State<ReceiptBookPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: defaultPadding*2),
+          children: [
+            DHDishCard(),
+            DHDishCard(),
+            DHDishCard(),
+          ],
+        ),
+      ),
+    );
   }
 }
