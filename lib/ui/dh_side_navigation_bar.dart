41e4d68e8dcd52ff7c913049c3ed66d28456293f
marinalvdgs <marinalvdgs@gmail.com>
2021-03-11 18:06:38 +0600

    add simple animation and correct ui

--- lib/ui/dh_side_navigation_bar.dart
+++ lib/ui/dh_side_navigation_bar.dart
@@ -1,4 +1,5 @@
-import 'package:dapur_hangus_app/ui/dh_navigation_button.dart';
+import 'package:dapur_hangus_app/theme.dart';
+import 'package:dapur_hangus_app/ui/dh_navigation_item.dart';
 import 'package:flutter/material.dart';
 
 class DHSideNavigationBar extends StatefulWidget {
@@ -24,19 +25,33 @@ class _DHSideNavigationBarState extends State<DHSideNavigationBar> {
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).primaryColor,
-      width: 90,
+      width: 80,
       height: MediaQuery.of(context).size.height,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: widget.items
-            .map((e) => DHNavigationButton(
-                  title: e,
-                  isSelected: widget.currentIndex == widget.items.indexOf(e),
-                  onTap: () => onItemTap(widget.items.indexOf(e)),
-                ))
-            .toList(),
-      ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: defaultPadding * 3),
+              child: Image.asset(
+                'assets/setup-dots.png',
+                color: Theme.of(context).scaffoldBackgroundColor,
+                height: 24,
+              ),
+            ),
+            Expanded(child: Container()),
+            ...widget.items
+                .map((e) => DHNavigationItem(
+                      title: e,
+                      isSelected:
+                          widget.currentIndex == widget.items.indexOf(e),
+                      onTap: () => onItemTap(widget.items.indexOf(e)),
+                    ))
+                .toList(),
+            SizedBox(
+              height: 60,
+            )
+          ]),
     );
   }
 }
